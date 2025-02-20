1dcddac9ab4f25a28d5a7cce589918fb43165249
Gershom Ndolo <gashadogo@gmail.com>
2025-02-20 15:08:42 +0300

    Firebase
    Switch from mirage.js to firebase db

---
+++ api.js
@@ -0,0 +1,87 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc,  getDocs, getDoc, query, where } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARN_Fnwtzoie6tBV4-P2Hf9EBEhQPxLAg",
+  authDomain: "vanlife-a769b.firebaseapp.com",
+  projectId: "vanlife-a769b",
+  storageBucket: "vanlife-a769b.firebasestorage.app",
+  messagingSenderId: "201920379489",
+  appId: "1:201920379489:web:8c82c399eabac8b3ed8820"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id )
+    const snapshot = await getDoc(docRef)
+    return ({...snapshot.data(), id: snapshot.id})
+}
+
+export async function getHostVans(id) {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+/*export async function getVans(id) {
+    const url = id ? `/api/vans/${id}` : "/api/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}*/
+
+/*export async function getHostVans(id) {
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}*/
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
